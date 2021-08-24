62e4c66d53dc8f765c6b34dbd0beb52c84fc24e1
pedro-git-projects <pedro.coding.contact@gmail.com>
2021-08-24 19:26:54 -0300

    added files via upload

---
+++ fundamentals/001.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Olá C++";
+    return 0;
+}
---
+++ fundamentals/002.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+
+int main()
+{
+	{ //blocos de código agrupam sentenças de código
+		; //uma sentença é a unidade sintática de uma linguagem de programação imperativa
+		;
+	}
+	
+	std::cout <<
+		"Olá!"
+		<< 
+		" Tchau!"
+		<<
+		std::endl;
+
+	std::cout << "fim!";
+	return 0;
+}
